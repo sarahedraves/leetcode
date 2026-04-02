dddc56a3afbe5caabb95fedbe17ffac5e69b41a6
Sarah Draves <sarah@draves.org>
2026-04-01 20:37:22 -0400

    Time: 0 ms (100%), Space: 20.5 MB (46.81%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -6,17 +6,26 @@
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head is None:
-            return None
-    #     return recursive
+            return None        
 
-    # def recursive(self, curnode, lastval)
-    #     if curnode.next is None:
+        #basic iteration
+        # newnode=ListNode(head.val,None)
+        # curnode=head
+        # while curnode.next is not None:
+        #     curnode=curnode.next
+        #     newnode=ListNode(curnode.val,newnode)
         
+        # return newnode
 
-        newnode=ListNode(head.val,None)
-        curnode=head
-        while curnode.next is not None:
-            curnode=curnode.next
-            newnode=ListNode(curnode.val,newnode)
+        #improved iteration with pointer changes instead of making new nodes
+        if head.next is None:
+            return head
         
-        return newnode
+        prev=None
+        cur=head
+        while cur is not None:
+            next_temp=cur.next #save pointer to next node
+            cur.next=prev #reverse pointer on the current node
+            cur,prev=next_temp,cur #move current to the saved reference to the next node and previous to the old current node
+        
+        return prev #returning prev because cur is none and prev is the last node that now points the right way
